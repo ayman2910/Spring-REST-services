4c6b9ccccad3be59f7cbe1c698573b3d58e52861
Hossain Ayman <ayman.dev2003@gmail.com>
2026-06-27 18:34:59 +0600

    Day 2 REST services

--- demo/src/main/java/com/SpringRESTapis/demo/Controllers/StudentController.java
+++ demo/src/main/java/com/SpringRESTapis/demo/Controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.SpringRESTapis.demo.Controllers;
 
 import com.SpringRESTapis.demo.POJOs.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +13,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    List<Student> students;
+    @PostConstruct
+    private void loadData(){
+        students = new ArrayList<>();
 
         students.add(new Student(1,"Hossain","Ayman"));
         students.add(new Student(2,"Suhaila","Obdhi"));
         students.add(new Student(3,"Nigga","Higga"));
 
+        System.out.println("Loading Data");
+
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
     }
     @GetMapping("student/{studentId}")
-    public Student getStudent(@PathVariable("studentId") int studentId){
-        List<Student> students = new ArrayList<>();
-
-        students.add(new Student(1,"Hossain","Ayman"));
-        students.add(new Student(2,"Suhaila","Obdhi"));
-        students.add(new Student(3,"Nigga","Higga"));
-
+    public Student getStudent(@PathVariable int studentId){
         return students.get(studentId-1);
     }
 
